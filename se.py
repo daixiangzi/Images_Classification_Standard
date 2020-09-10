08c1fd7c42cacb300e487a322d496a45907de30d
daixiangzi <543826458@qq.com>
2020-09-10 12:39:09 +0800

    Create se.py

---
+++ se.py
@@ -0,0 +1,18 @@
+class SeModule(nn.Module):
+    def __init__(self, in_size, reduction=4):
+        super(SeModule, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_size, in_size // reduction, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size // reduction),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_size // reduction, in_size, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size),
+            hsigmoid()
+        )
+
+    def forward(self, x):
+        return x * self.se(x)
+   
+   #input：1*96*112*112，MAC:0.001209 G,params:0.004848M
+   #use:se_model = SeModule(96),x = se_model(x)
